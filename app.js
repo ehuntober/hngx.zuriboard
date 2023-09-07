38ea4972a73f03d1f3065fe63c5d9e9a96324ca0
ehuntober <ehuntercrypto@gmail.com>
2023-09-07 14:42:51 +0000

    app modified

--- app.js
+++ app.js
@@ -8,20 +8,31 @@ const port = process.env.PORT || 3000;
 app.get('/api', (req, res) => {
     const { slack_name, track } = req.query;
 
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const currentDate = new Date();
-    const currentDay = daysOfWeek[currentDate.getUTCDay()];
-  
+      // Get the current day of the week
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const currentDate = new Date();
+  const currentDay = daysOfWeek[currentDate.getUTCDay()];
 
-    const now = new Date();
-    now.setMinutes(now.getMinutes() + Math.floor(Math.random() * 5) - 2);
-    const utc_time = now.toISOString();
+  // Calculate the +/-2 minute time window
+  const now = new Date();
+  const minTime = new Date(now);
+  minTime.setUTCMinutes(minTime.getUTCMinutes() - 2);
+  const maxTime = new Date(now);
+  maxTime.setUTCMinutes(maxTime.getUTCMinutes() + 2);
+
+  // Check if the current time is within the allowed window
+  if (now < minTime || now > maxTime) {
+    return res.status(500).json({ error: 'Time validation failed' });
+  }
+
+  // Get the current UTC time in the specified format
+  const formattedUtcTime = now.toISOString().slice(0, 19) + 'Z';
   
    
     const response = {
       slack_name,
       current_day: currentDay,
-      utc_time,
+      utc_time: formattedUtcTime,
       track,
       github_file_url: 'https://github.com/ehuntober/hngx.zuriboard/blob/master/app.js',
       github_repo_url: 'https://github.com/ehuntober/hngx.zuriboard',
