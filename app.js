871a9299c9e1126b46a81df01360dbac7a4d178d
ehuntober <ehuntercrypto@gmail.com>
2023-09-07 14:40:52 +0000

    updated the files

--- app.js
+++ app.js
@@ -8,28 +8,31 @@ const port = process.env.PORT || 3000;
 app.get('/api', (req, res) => {
     const { slack_name, track } = req.query;
 
- const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
- const currentDate = new Date();
- const currentDay = daysOfWeek[currentDate.getUTCDay()];
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDate = new Date();
+    const currentDay = daysOfWeek[currentDate.getUTCDay()];
+  
 
+    const now = new Date();
+    now.setMinutes(now.getMinutes() + Math.floor(Math.random() * 5) - 2);
+    const utc_time = now.toISOString();
+  
+   
+    const response = {
+      slack_name,
+      current_day: currentDay,
+      utc_time,
+      track,
+      github_file_url: 'https://github.com/ehuntober/hngx.zuriboard/blob/master/app.js',
+      github_repo_url: 'https://github.com/ehuntober/hngx.zuriboard',
+      status_code: 200,
+    };
+  
+    res.json(response);
+  });
+  
 
- const now = new Date();
- const formattedUtcTime = now.toISOString().slice(0, 19) + 'Z';
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 
- const response = {
-   slack_name,
-   current_day: currentDay,
-   utc_time: formattedUtcTime,
-   track,
-   github_file_url: 'https://github.com/username/repo/blob/main/file_name.ext',
-   github_repo_url: 'https://github.com/username/repo',
-   status_code: 200,
- };
-
- res.json(response);
-});
-
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
