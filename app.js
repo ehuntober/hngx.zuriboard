54d7a12f63254aab82a0d46828bcdcf9677d41ba
ehuntober <ehuntercrypto@gmail.com>
2023-09-07 13:41:34 +0000

    modified the app.js file

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-const express = require('express')
 
+const express = require('express')
 const app = express();
 
-app.get('/',(req,res) =>{
-    res.send('App is live!')
-
-})
+const port = process.env.PORT || 3000;
 
 
-app.listen(3000, ()=>{
-    console.log('Running on port 3000')
-})
+app.get('/api', (req, res) => {
+    const { slack_name, track } = req.query;
+
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDate = new Date();
+    const currentDay = daysOfWeek[currentDate.getUTCDay()];
+  
+
+    const now = new Date();
+    now.setMinutes(now.getMinutes() + Math.floor(Math.random() * 5) - 2);
+    const utc_time = now.toISOString();
+  
+   
+    const response = {
+      slack_name,
+      current_day: currentDay,
+      utc_time,
+      track,
+      github_file_url: 'https://github.com/ehuntober/hngx.zuriboard/blob/master/app.js',
+      github_repo_url: 'https://github.com/ehuntober/hngx.zuriboard',
+      status_code: 200,
+    };
+  
+    res.json(response);
+  });
+  
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
