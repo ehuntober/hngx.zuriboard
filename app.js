44b84f9c9db2b7f95a55c23e77d6af31fecb93d9
ehuntober <ehuntercrypto@gmail.com>
2023-09-07 14:47:11 +0000

    fixed utc_time error

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ const port = process.env.PORT || 3000;
 app.get('/api', (req, res) => {
     const { slack_name, track } = req.query;
 
-      // Get the current day of the week
+ 
   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const currentDate = new Date();
   const currentDay = daysOfWeek[currentDate.getUTCDay()];
 
-  // Calculate the +/-2 minute time window
+
   const now = new Date();
   const minTime = new Date(now);
   minTime.setUTCMinutes(minTime.getUTCMinutes() - 2);
   const maxTime = new Date(now);
   maxTime.setUTCMinutes(maxTime.getUTCMinutes() + 2);
 
-  // Check if the current time is within the allowed window
+
   if (now < minTime || now > maxTime) {
     return res.status(500).json({ error: 'Time validation failed' });
   }
 
-  // Get the current UTC time in the specified format
+
   const formattedUtcTime = now.toISOString().slice(0, 19) + 'Z';
   
    
